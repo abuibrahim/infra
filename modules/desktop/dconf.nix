0f6db3e2dc325dadcdcb59be80572a34d9e11030
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2025-09-02 23:31:03 -0700

    fix dconf

--- modules/desktop/dconf.nix
+++ modules/desktop/dconf.nix
@@ -1,9 +1,16 @@
 {
-  flake.modules.homeManager.dconf = {
-    enable = true;
-    settings = {
+  flake.modules.nixos.desktop = {
+    programs.dconf.enable = true;
+  };
+
+  flake.modules.homeManager.desktop = {
+    dconf.settings = {
       "org/gnome/desktop/interface" = {
         monospace-font-name = "JetBrainsMono NF 12";
+      };
+      "org/gnome/desktop/default-applications/terminal" = {
+        exec = "ghostty";
+        exec-arg = "-e";
       };
       "org/gnome/desktop/wm/preferences" = {
         focus-mode = "sloppy";
