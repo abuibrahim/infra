80b54ef83bc2f4c6429849387651b2d0d9c8958f
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2025-09-06 12:40:02 -0700

    niri: enable xwayland-sattelite

--- modules/desktop/niri/default.nix
+++ modules/desktop/niri/default.nix
@@ -1,9 +1,14 @@
 {
   flake.modules = {
-    nixos.desktop = {
-      programs.niri.enable = true;
-      environment.sessionVariables.NIXOS_OZONE_WL = "1";
-    };
+    nixos.desktop =
+      { pkgs, ... }:
+      {
+        programs.niri.enable = true;
+        environment.sessionVariables.NIXOS_OZONE_WL = "1";
+        environment.systemPackages = with pkgs; [
+          xwayland-satellite
+        ];
+      };
 
     homeManager.desktop = {
       xdg.configFile."niri/config.kdl".source = ./config.kdl;
