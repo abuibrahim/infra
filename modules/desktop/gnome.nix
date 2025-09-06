06fdc4a610962702d5e2976c3585f6bd8faf8f39
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2025-09-06 12:54:31 -0700

    gnome: re-enable gnome core-apps

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -1,28 +1,12 @@
 {
-  flake.modules.nixos.desktop =
-    { pkgs, ... }:
-    {
-      services.displayManager.gdm.enable = true;
-      services.desktopManager.gnome = {
-        enable = true;
-        extraGSettingsOverrides = ''
-          [org.gnome.mutter]
-          experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
-        '';
-      };
-
-      services.gnome.core-apps.enable = false;
-
-      environment.gnome.excludePackages = with pkgs; [
-        gnome-tour
-      ];
-
-      environment.systemPackages = with pkgs; [
-        loupe
-        nautilus
-        gnome-calendar
-        file-roller
-        seahorse
-      ];
+  flake.modules.nixos.desktop = {
+    services.displayManager.gdm.enable = true;
+    services.desktopManager.gnome = {
+      enable = true;
+      extraGSettingsOverrides = ''
+        [org.gnome.mutter]
+        experimental-features=['scale-monitor-framebuffer', 'xwayland-native-scaling']
+      '';
     };
+  };
 }
