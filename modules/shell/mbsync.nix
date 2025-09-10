3858423c021b6a0655daa1b4577e2016c6c80094
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2025-09-10 01:38:17 -0700

    home: shell: add mbsync

---
+++ modules/shell/mbsync.nix
@@ -0,0 +1,74 @@
+let
+  extraConfig = {
+    CopyArrivalDate = "yes";
+    Create = "both";
+    Expunge = "both";
+    SyncState = "*";
+  };
+in
+{
+  flake.modules.homeManager.shell = {
+    programs.mbsync.enable = true;
+
+    accounts.email.accounts.Apple.mbsync = {
+      enable = true;
+      create = "both";
+      expunge = "both";
+      groups.Apple.channels = {
+        all = {
+          patterns = [
+            "INBOX"
+            "Archive"
+            "Trash"
+            "Spam"
+            "Drafts"
+          ];
+          inherit extraConfig;
+        };
+        sent = {
+          farPattern = "Sent Messages";
+          nearPattern = "Sent";
+          inherit extraConfig;
+        };
+      };
+    };
+
+    accounts.email.accounts.Gmail.mbsync = {
+      enable = true;
+      create = "both";
+      expunge = "both";
+      groups.Gmail.channels = {
+        inbox = {
+          farPattern = "INBOX";
+          nearPattern = "Inbox";
+          inherit extraConfig;
+        };
+        sent = {
+          farPattern = "[Gmail]/Sent Mail";
+          nearPattern = "Sent";
+          inherit extraConfig;
+        };
+        drafts = {
+          farPattern = "[Gmail]/Drafts";
+          nearPattern = "Drafts";
+          inherit extraConfig;
+        };
+        all = {
+          farPattern = "[Gmail]/All Mail";
+          nearPattern = "Archive";
+          inherit extraConfig;
+        };
+        spam = {
+          farPattern = "[Gmail]/Spam";
+          nearPattern = "Spam";
+          inherit extraConfig;
+        };
+        trash = {
+          farPattern = "[Gmail]/Trash";
+          nearPattern = "Trash";
+          inherit extraConfig;
+        };
+      };
+    };
+  };
+}
