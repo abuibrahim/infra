6c46882dc63dce57e0953ee8980db281e5c999c2
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2026-03-31 12:34:19 -0700

    dms-shell: add Calculator plugin

--- modules/desktop/dms.nix
+++ modules/desktop/dms.nix
@@ -4,6 +4,17 @@
     {
       programs.dms-shell = {
         enable = true;
+        plugins = {
+          Calculator = {
+            enable = true;
+            src = pkgs.fetchFromGitHub {
+              owner = "rochacbruno";
+              repo = "DankCalculator";
+              rev = "0.2.2";
+              sha256 = "sha256-bhV22bL38CJp58Y8tCY8sEBRYxmuk671fEymmdg0Yuk=";
+            };
+          };
+        };
       };
       services.displayManager.dms-greeter = {
         enable = true;
