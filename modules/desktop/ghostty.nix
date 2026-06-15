ac96b9b6d2ff770ec193833f9857168858b75dc2
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2026-06-15 12:19:09 -0700

    ghostty: remove font settings

--- modules/desktop/ghostty.nix
+++ modules/desktop/ghostty.nix
@@ -3,8 +3,6 @@
     programs.ghostty = {
       enable = true;
       settings = {
-        font-family = "JetBrainsMono NF";
-        font-size = 11;
         background-opacity = 0.9;
         background-blur-radius = 20;
         background-blur = true;
