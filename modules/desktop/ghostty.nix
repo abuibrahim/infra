19a28b78eca65414098f8b047e018d6ef19d90c5
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2026-06-20 14:12:55 -0700

    ghostty: change background-opacity to 0.9 -> 0.85

--- modules/desktop/ghostty.nix
+++ modules/desktop/ghostty.nix
@@ -3,7 +3,7 @@
     programs.ghostty = {
       enable = true;
       settings = {
-        background-opacity = 0.9;
+        background-opacity = 0.85;
         background-blur-radius = 20;
         background-blur = true;
         link-url = true;
