2822a25bfff8fdd263671c2bb14a0dc2253d84f9
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2026-06-20 16:50:32 -0700

    ghostty: fix config

--- modules/desktop/ghostty.nix
+++ modules/desktop/ghostty.nix
@@ -3,8 +3,8 @@
     programs.ghostty = {
       enable = true;
       settings = {
+        copy-on-select = true;
         background-opacity = 0.85;
-        background-blur-radius = 20;
         background-blur = true;
         link-url = true;
       };
