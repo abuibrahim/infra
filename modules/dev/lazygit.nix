f60f634188456590cd565e0176d5704a8b3fb158
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2026-09-03 20:58:59 -0700

    lazygit: use delta as pager

--- modules/dev/lazygit.nix
+++ modules/dev/lazygit.nix
@@ -1,5 +1,18 @@
 {
-  flake.modules.homeManager.dev = {
-    programs.lazygit.enable = true;
+  flake.modules.homeManager.dev = { config, lib, ... }: {
+    programs.lazygit = {
+      enable = true;
+      settings = {
+        git = {
+          skipDiscardChangeWarning = true;
+          pagers = [
+            {
+              colorArg = "always";
+              pager = "${lib.getExe config.programs.delta.package} --dark --paging=never";
+            }
+          ];
+        };
+      };
+    };
   };
 }
