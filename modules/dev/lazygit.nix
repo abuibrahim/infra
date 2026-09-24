0063973422d5b555f18c3b9ff2c7d11dddf87028
Abdurrahman Hussain <2321000+abuibrahim@users.noreply.github.com>
2026-09-23 17:54:16 -0700

    lazygit: disable prompt when returning from sub-process

--- modules/dev/lazygit.nix
+++ modules/dev/lazygit.nix
@@ -3,6 +3,7 @@
     programs.lazygit = {
       enable = true;
       settings = {
+        promptToReturnFromSubprocess = false;
         git = {
           skipDiscardChangeWarning = true;
           diffRenderers = [
